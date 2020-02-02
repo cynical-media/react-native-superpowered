1f6128ce3aeebd9fb7a7446e0e4f2eae40e449a8
Chris Fogelklou <chris.fogelklou@polestar.com>
2020-02-02 15:46:54 +0100

    Move

---
+++ src_xplatform/json_commands/json_to_superpowered.hpp
@@ -0,0 +1,10 @@
+#ifndef JSON_TO_SUPERPOWERED_HPP
+#define JSON_TO_SUPERPOWERED_HPP
+
+#include <string>
+
+// Sends a json command, gets a response.
+std::string jsonCommand(std::string &cmd);
+
+
+#endif
