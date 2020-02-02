64be22eed7b3ce5bafacac876f9e199eb6e45678
Chris Fogelklou <chris.fogelklou@polestar.com>
2020-02-02 15:47:43 +0100

    Move

--- src_xplatform/json_to_superpowered.cpp
+++
@@ -1,9 +0,0 @@
-#include "json_to_superpowered.hpp"
-#include "nlohmann/json.hpp"
-
-using namespace nlohmann;
-
-// Sends a json command, gets a response.
-std::string jsonCommand(std::string &cmd){
-  // TODO, parse json and issue commands to superpowered.
-}
